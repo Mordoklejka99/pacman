0ebdf8cb56e6e80fbbe3f5803165a3008e68f303
Mordoklejka <marcing305@wp.pl>
2019-10-04 23:38:01 +0200

    rm

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ src/*.cpp -lsfml-graphics -lsfml-window -lsfml-system -o pacman -g
--- src/headers/game.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef GAME_HPP
-#define GAME_HPP
-
-#include <SFML/Graphics.hpp>
-
-int game(sf::RenderWindow& window);
-
-#endif
